2d8e436e3d8412f86b43569e1db43cf9d4f4626a
Sly_tom_cat <slytomcat@mail.ru>
2017-12-24 16:37:28 +0300

    SetIcon by path to file (not by buffer with image bytes)

--- systray.h
+++ systray.h
@@ -3,7 +3,7 @@ extern void systray_on_exit();
 extern void systray_menu_item_selected(int menu_id);
 int nativeLoop(void);
 
-void setIcon(const char* iconBytes, int length);
+void setIcon(char* icon_file_name);
 void setTitle(char* title);
 void setTooltip(char* tooltip);
 void add_or_update_menu_item(int menuId, char* title, char* tooltip, short disabled, short checked);
